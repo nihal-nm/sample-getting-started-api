266d8905e54e4dc90710a3a558933188b6e1c04f
Nihal NM <nihal.nm@ibm.com>
2025-07-06 21:56:14 +0530

    Update SampleApplication.java
    Update SampleApplication.java

--- src/main/java/io/openliberty/sample/SampleApplication.java
+++ src/main/java/io/openliberty/sample/SampleApplication.java
@@ -14,6 +14,7 @@ package io.openliberty.sample;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+
 @ApplicationPath("system")
 public class SampleApplication extends Application {
 }
